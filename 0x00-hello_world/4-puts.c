5f6626db816098f66900e72fc6da2458a5a8bc9e
cily-codes <okoroamarachicecilia@gmail.com>
2023-04-20 10:29:07 -0700

    puts C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
-/*
- * Main - entry point of the program.
- *
- * Description: This program prints a message to the system.
- *
- * Return: Always 0.
- */
 int main(void)
 {
 puts("Programming is like building a multilingual puzzle);
-return (0);
+return 0;
 }
