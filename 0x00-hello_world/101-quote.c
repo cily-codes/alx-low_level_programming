3aa08c3d2d2947afb666ca9a8461f26dec4ec170
cily-codes <okoroamarachicecilia@gmail.com>
2023-04-20 17:05:04 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Entry point
  *
- * Return: 0 (sucess)
+ * Return: 1 (sucess)
  */
 int main(void)
 {
-	const char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDERR_FILENO, str, 59);
-	return 1;
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
