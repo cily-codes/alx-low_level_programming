3e751db1740da88dae95bb816e172bfb281a899d
cily-codes <okoroamarachicecilia@gmail.com>
2023-04-21 17:51:27 -0700

    combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,22 +7,19 @@
  */
 int main(void)
 {
-	int game, boy;
+	int game = 0;
 
 	for (game = 0; game < 10; game++)
 	{
-		for (boy = game; boy < 10; boy++)
+		putchar (game + '0');
+		if (game != 9)
 		{
-			putchar (game + '0');
-			if (game != 9 || boy != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-			else
-			{
-				putchar ('\n');
-			}
+			putchar(',');
+			putchar(' ');
+		}
+		else
+		{
+			putchar ('\n');
 		}
 	}
 	return (0);
