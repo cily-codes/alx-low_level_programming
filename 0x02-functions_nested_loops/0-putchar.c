c88cdb1a5612314cf8a338eff6c9b1187c58ba6b
cily-codes <okoroamarachicecilia@gmail.com>
2023-04-26 04:48:45 -0700

    Print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,34 +1,22 @@
-#include <unistd.h>
+#include "main.h"
 #include <stdio.h>
-
-/**
- * my_putchar - writes the character H to stdout
- * @H: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int my_putchar(char H)
-{
-	return (write(1, &H, 1));
-}
 
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 
 int main(void)
 {
-	char words[] = "_putchar";
-	int length = sizeof(words) / sizeof(words[0]);
-	int T;
-
-	for (T = 0; length > T; T++)
-	{
-		putchar(words[T]);
-	}
-
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 	return (0);
 }
