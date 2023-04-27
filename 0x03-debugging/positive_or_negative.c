c2b5090ceaa8af48825a008eacedecf63b7bc06c
cily-codes <okoroamarachicecilia@gmail.com>
2023-04-27 15:12:05 -0700

    upate

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- * positive_or_negative - prints whether a number is positive or negative
- *
- *@i: random number
+ * main - prints whether a random number is positive or negative
  *
  * Return: Always 0 (Success)
- */
+*/
 
 void positive_or_negative(int i)
 {
-	if (i > 0)
-		printf("%d is positive\n", i);
-	else if (i < 0)
-		printf("%d is negative\n", i);
-	else
-		printf("%d is zero\n", i);
+        if (i > 0)
+                printf("%d is positive\n", i);
+        else if (i < 0)
+                printf("%d is negative\n", i);
+        else
+                printf("%d is zero\n", i);
 }
