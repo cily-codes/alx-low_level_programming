3f1f4c973b0187dd5c5aedb9ca3f8768f9e48376
cily-codes <okoroamarachicecilia@gmail.com>
2023-04-28 07:17:15 -0700

    print number 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from '0' to '9'
+ *
+ * Return: requested data followed by new line
+ */
+void print_numbers(void)
+{
+	int l
+
+	for (l = 0, l <= 9, l++)
+	{
+		_putchar(l +'0');
+	}
+
+	_putchar('\n');
+}
