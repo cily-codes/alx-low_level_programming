cfaac57f2b6c682c306ec4062b1301e9f2924970
cily-codes <okoroamarachicecilia@gmail.com>
2023-04-28 10:02:43 -0700

    print a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ *
+ * @n: the number of times the character _ should be printed
+ *
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int l;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (l = 0; l < n; l++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
