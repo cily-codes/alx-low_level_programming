3cd1cc1203b308fed39cf21c0eb1e015274ddc35
cily-codes <okoroamarachicecilia@gmail.com>
2023-04-28 18:22:33 -0700

    print an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,14 +9,23 @@
  */
 void print_number(int n)
 {
-	if (n < 0)
+	if (n > -10 && n < 10)
 	{
-		_putchar('-');
-		n *= -1;
+		if (n < 0)
+		{
+			_putchar('-');
+			n = n * -1;
+		}
+		_putchar(n + '0');
 	}
-	if (n / 10)
+	else
 	{
 		print_number(n / 10);
+		n = n % 10;
+
+		if (n < 0)
+			n = n * -1;
+
+		_putchar(n + '0');
 	}
-	_putchar((n % 10) + '0');
 }
