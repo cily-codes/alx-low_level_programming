9a8f1e9a45ad1ed2b390632eaf094d68504a52b6
cily-codes <okoroamarachicecilia@gmail.com>
2023-05-02 08:50:30 -0700

    Swap Integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ *
+ * @a: integer to swap with b
+ * @b: integer to swap with a
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int swp;
+
+	swp = *a;
+	*a = *b;
+	*b = swp;
+}
