9526a1bd21a4a37293817daba1c22775ff6f54ee
cily-codes <okoroamarachicecilia@gmail.com>
2023-05-02 08:18:43 -0700

    update value of parameter with pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates n to 98
+ *
+ * @n: parameter to be updated
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Continuation of C; Pointers, Arrays, & Strings
