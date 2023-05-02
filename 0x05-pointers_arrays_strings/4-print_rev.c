fbfaa4201dd2f41f8a23d505b7900e60e9b8a598
cily-codes <okoroamarachicecilia@gmail.com>
2023-05-02 16:33:13 -0700

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: string to be printed in reverse
+ *
+ * Return: String in reverse
+ */
+void print_rev(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	print_rev(s + 1);
+	_putchar(*s);
+}
