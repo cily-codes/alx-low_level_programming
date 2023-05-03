f633aefd9f54c133becf5e037564b1606e34d252
cily-codes <okoroamarachicecilia@gmail.com>
2023-05-02 18:09:38 -0700

    print un-skipped letter

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ *
+ * @str: string to print
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int a;
+	int b = 0;
+
+	while (str[b] != '\0')
+	{
+		b++;
+	}
+
+	for (a = 0; a < b; a += 2)
+	{
+		_putchar(str[a]);
+	}
+
+	_putchar('\n');
+}
