70c213f8fef74e1d51bf1b4f08e73ac7e4f073db
cily-codes <okoroamarachicecilia@gmail.com>
2023-05-04 21:40:47 -0700

    infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -13,22 +13,40 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int tom, jerry;
+	int a = 0, b = 0, lc, ln1, ln2, rem = 0, cur_sum;
 
-	while (n1[tom] != '\0')
-		tom++;
-	while (n2[jerry] != '\0')
-		jerry++;
-
-	*r = *(r + size_r);
-	while (n1[tom] > 0 || n1[jerry] > 0)
+	while (n1[a] != '\0')
+		a++;
+	while (n2[b] != '\0')
+		b++;
+	lc = a >= b ? a : b;
+	if (size_r <= lc + 1)
+		return (0);
+	r[lc + 1] = '\0';
+	a--, b--, size_r--;
+	ln1 = n1[a] - '0', ln2 = n2[b] - '0';
+	while (lc >= 0)
 	{
-		if (n1[tom] + n2[jerry] > 0)
-			*r = n1[tom - 1] + n2[jerry - 1] + 1;
+		cur_sum = ln1 + ln2 + rem;
+		if (cur_sum > 9)
+			rem = cur_sum / 10;
 		else
-			*r = n1[tom] + n2[jerry];
-		tom--;
-		jerry--;
+			rem = 0;
+		if (cur_sum > 0)
+			r[lc] = (cur_sum % 10) + 48;
+		else
+			r[lc] = '0';
+		if (a > 0)
+			a--, ln1 = n1[a] - 48;
+		else
+			ln1 = 0;
+		if (b > 0)
+			b--, ln2 = n2[b] - 48;
+		else
+			ln2 = 0;
+		lc--, size_r--;
 	}
+	if (*r == '0')
+		return (r + 1);
 	return (r);
 }
