6c308c99d0ccc55a4c982e4a3602d5bb8fdf52de
cily-codes <okoroamarachicecilia@gmail.com>
2023-05-12 09:26:20 -0700

    Print your Tenants name

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+
+	for (a = 0; a < argc; a++)
+	{
+		printf("%s\n", argv[a]);
+	}
+
+	return (0);
+}
