40e32ea01ce99e5957fa2f669dd534cfcb45a569
cily-codes <okoroamarachicecilia@gmail.com>
2023-05-19 15:19:28 -0700

    multiply integers

---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character l to stdout
+ * @l: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char l)
+{
+	return (write(1, &l, 1));
+}
