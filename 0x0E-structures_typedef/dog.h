dcffb2495f918e07088e75472a918440c8075952
cily-codes <okoroamarachicecilia@gmail.com>
2023-05-22 16:54:13 -0700

    create struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - dog type
+ *
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
