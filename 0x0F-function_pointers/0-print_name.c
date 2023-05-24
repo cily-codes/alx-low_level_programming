73cb3be7804bf082dc373b7ca9a389d6ac1124c4
cily-codes <okoroamarachicecilia@gmail.com>
2023-05-24 08:04:42 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ *
+ * @name: name to print out
+ * @f: function to pointer
+ *
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
+}
