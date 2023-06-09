42b8f34b6574af0467d9d1167f1de4dedd7f7556
cily-codes <okoroamarachicecilia@gmail.com>
2023-06-08 19:06:53 -0700

    try to fix Clear bits output fail

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,7 +12,8 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index < sizeof(unsigned long int) * 8)
 	{
-		return ((*n &= ~1 << index) ? 1 : -1);
+		*n &= (~(1 << index));
+		return (1);
 	}
 
 	return (-1);
