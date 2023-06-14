ca05d5f7879713232e7eaaac693b3c9b58d60ed4
cily-codes <okoroamarachicecilia@gmail.com>
2023-06-13 19:24:00 -0700

    program that copies file content (pescriptor)

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define BUFFER_SIZE 1024
+
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/uio.h>
@@ -14,6 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int _strlen(const char *s);
 int append_text_to_file(const char *filename, char *text_content);
-
+int main(int argc, char *argv[]);
+void file_copy(const char *file_from, const char *file_to);
 
 #endif
