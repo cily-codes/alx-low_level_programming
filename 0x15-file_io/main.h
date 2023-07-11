9e28d9b275d9c9a8047e86ea08f5cb0da7e34f3c
cily-codes <okoroamarachicecilia@gmail.com>
2023-07-10 17:13:55 -0700

    copies file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define BUFFER_SIZE 1024
 
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -15,6 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _strlen(const char *s);
-
+int main(int argc, char *argv[]);
+void file_copy(const char *file_from, const char *file_to);
 
 #endif
