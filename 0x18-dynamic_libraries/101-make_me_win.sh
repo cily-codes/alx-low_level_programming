39a7eca6a3decd3f2f35ef842c11315787db468b
cily-codes <okoroamarachicecilia@gmail.com>
2023-08-23 17:18:55 -0700

    trying to fix the check 4 issue

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -shared -o myprint.so -fPIC giga_test.c
-export LD_PRELOAD=./myprint.so:$LD_LIBRARY_PATH
+gcc -shared -o /alx-low_level_programming/0x18-dynamic_libraries/giga_test.so -fPIC giga_test.c
+export LD_PRELOAD=/alx-low_level_programming/0x18-dynamic_libraries/giga_test.so:$LD_LIBRARY_PATH
