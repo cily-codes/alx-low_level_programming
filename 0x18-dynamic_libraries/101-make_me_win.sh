516a7ef900242c595b2d32cd4f2b8c683f7e79e3
cily-codes <okoroamarachicecilia@gmail.com>
2023-08-23 17:28:48 -0700

    dont't know what i'm doing wrong

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -shared -o myprint.so -fPIC giga_test.c
-export LD_PRELOAD=./myprint.so:$LD_LIBRARY_PATH
+gcc -shared -o giga_test.so -fPIC giga_test.c
+export LD_PRELOAD=./giga_test.so:$LD_LIBRARY_PATH
